c10bf9b745402c66df252b0bd559885aec84a9e4
zhaoleiwang <johnsonli_drum@163.com>
2017-12-15 02:08:35 -0500

    all files can upload

--- flask_start.py
+++ flask_start.py
@@ -1,31 +1,24 @@
 # -*- coding: utf-8 -*-
 import os
 
-from flask import Flask, render_template
-
-from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class, ALL
+from flask import Flask, render_template, url_for
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'I have a dream'
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd()
 
-
-#app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd()
-Dest = '/Users/zhaoleiwang/PycharmProjects/CloudDisk/myfile'
-#app.config['UPLOADED_PHOTOS_DEST'] = Dest
-app.config['UPLOADS_DEFAULT_DEST'] = Dest
-
-#photos = UploadSet('photos', IMAGES)
-files = UploadSet('files',ALL)
-configure_uploads(app, files)
-patch_request_class(app,100 *1024 * 1024)  # set maximum file size, default is 16MB
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)  # set maximum file size, default is 16MB
 
 
 class UploadForm(FlaskForm):
-    file = FileField(validators=[
-        FileAllowed(files, u'！'),
+    photo = FileField(validators=[
+        FileAllowed(photos, u'只能上传图片！'),
         FileRequired(u'文件未选择！')])
     submit = SubmitField(u'上传')
 
@@ -34,12 +27,12 @@ class UploadForm(FlaskForm):
 def upload_file():
     form = UploadForm()
     if form.validate_on_submit():
-        filename = files.save(form.file.data)
-        file_url = files.url(filename)
+        filename = photos.save(form.photo.data)
+        file_url = photos.url(filename)
     else:
         file_url = None
-    return render_template('upload.html', form=form, file_url=file_url)
+    return render_template('index.html', form=form, file_url=file_url)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host = '0.0.0.0')
